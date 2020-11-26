e24984b7fda482c209da84c096a658dd573e14da
jstarmer <josh.starmer@icloud.com>
2020-11-26 05:56:53 -0500

    debug app.js

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
 
+
+
+// if(document.querySelector(".nav-links")){
+//     alert("Hello");
+// }
+
 burger.addEventListener("click", () =>{
-    nav.classList.toggle("nav-open");
+    nav.classList.toggle(".nav-open");
 });
 
 
